b7bf1f5e5d0cb3125eac5dd31c9de2c35cd0de2e
WildanDesu <165839565+WildanDesu@users.noreply.github.com>
2025-03-17 04:24:39 +0800

    set_up.py

---
+++ set_up.py
@@ -0,0 +1,7 @@
+import os
+
+os.system("termux-setup-storage")
+os.system("pip install yt-dlp")
+os.system("pkg install ffmpeg -y")
+os.system("pip install instaloader")
+os.system("pip install tqdm")
